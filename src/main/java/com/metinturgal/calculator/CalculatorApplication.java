5b7d8fb0d97e9525dc8e7b2e0579de6292fc2bb1
VNGRS <metinturgal@gmail.com>
2022-09-24 22:06:01 +0300

    changed to graddle

--- src/main/java/com/metinturgal/calculator/CalculatorApplication.java
+++ src/main/java/com/metinturgal/calculator/CalculatorApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
-  public int sum(int a, int b){
-    return a + b;
-  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
