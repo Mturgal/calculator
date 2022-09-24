57ce99c9f1a39ef7574af8849f199e99f64cc0d2
VNGRS <metinturgal@gmail.com>
2022-09-24 17:18:22 +0300

    Add spring boot skeleton

---
+++ src/test/java/com/metinturgal/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metinturgal.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
